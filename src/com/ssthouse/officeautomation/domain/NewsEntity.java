d8f95e1c7d68c748260aaba9def1d8e03da8d177
ssthouse <ssthouse@163.com>
2017-05-23 08:36:25 +0800

    Add creater filed in NewsEntity

--- src/com/ssthouse/officeautomation/domain/NewsEntity.java
+++ src/com/ssthouse/officeautomation/domain/NewsEntity.java
@@ -10,67 +10,83 @@ import com.ssthouse.officeautomation.util.StringUtil;
 @Entity
 @Table(name = "news", schema = "office_automation", catalog = "")
 public class NewsEntity {
-    private int id;
-    private String title;
-    private String link;
+	private int id;
+	private String title;
+	private String link;
+	private String creater;
 
-    @Id
-    @Column(name = "id", nullable = false)
-    public int getId() {
-        return id;
-    }
+	@Id
+	@Column(name = "id", nullable = false)
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    @Basic
-    @Column(name = "title", nullable = false, length = 1000)
-    public String getTitle() {
-        return title;
-    }
+	@Basic
+	@Column(name = "title", nullable = false, length = 1000)
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    @Basic
-    @Column(name = "link", nullable = false, length = 1000)
-    public String getLink() {
-        return link;
-    }
+	@Basic
+	@Column(name = "link", nullable = false, length = 1000)
+	public String getLink() {
+		return link;
+	}
 
-    public void setLink(String link) {
-        this.link = link;
-    }
+	public void setLink(String link) {
+		this.link = link;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Basic
+	@Column(name = "creater", nullable = false, length = 30)
+	public String getCreater() {
+		return creater;
+	}
 
-        NewsEntity that = (NewsEntity) o;
+	public void setCreater(String creater) {
+		this.creater = creater;
+	}
 
-        if (id != that.id) return false;
-        if (title != null ? !title.equals(that.title) : that.title != null) return false;
-        if (link != null ? !link.equals(that.link) : that.link != null) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        return true;
-    }
+		NewsEntity that = (NewsEntity) o;
 
-    @Override
-    public int hashCode() {
-        int result = id;
-        result = 31 * result + (title != null ? title.hashCode() : 0);
-        result = 31 * result + (link != null ? link.hashCode() : 0);
-        return result;
-    }
-    
-    @Transient
-    public boolean isValid(){
-    	if(StringUtil.isEmpty(this.link, this.title)){
-    		return false;
-    	}
-    	return true;
-    }
+		if (id != that.id)
+			return false;
+		if (title != null ? !title.equals(that.title) : that.title != null)
+			return false;
+		if (link != null ? !link.equals(that.link) : that.link != null)
+			return false;
+
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = id;
+		result = 31 * result + (title != null ? title.hashCode() : 0);
+		result = 31 * result + (link != null ? link.hashCode() : 0);
+		return result;
+	}
+
+	@Transient
+	public boolean isValid() {
+		if (StringUtil.isEmpty(this.link, this.title)) {
+			return false;
+		}
+		return true;
+	}
 }
